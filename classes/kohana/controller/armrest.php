fefe60a9de1c043a39b4aa72becf10a1b577d630
Mathew Hammond <emptyhammond@gmail.com>
2012-10-26 13:40:03 +0100

    like

--- classes/kohana/controller/armrest.php
+++ classes/kohana/controller/armrest.php
@@ -154,7 +154,12 @@ class Kohana_Controller_ArmREST extends Controller_REST {
 			
 			foreach(array_intersect_key($_GET, $object->list_columns()) as $key => $value)
 			{
-				$query->and_where($key, '=', $value);
+				$query->and_where($key, 'LIKE', $value);
+			}
+			
+			if(isset($_GET['in']))
+			{
+				$query->and_where($object->primary_key(), 'IN', explode(',',$_GET['in']));
 			}
 			
 			if(isset($_GET['order']))
